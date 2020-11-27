39d3952520c4c5260b730073e561925ae7e06664
edoardocollo <edoardo.collo@gmail.com>
2020-11-27 16:16:40 +0100

    left arrow

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var frecciaDestra = $('.fa-angle-right');
-
+var frecciaSinistra = $('.fa-angle-left');
 frecciaDestra.click(function(){
   var  first = $('.first');
   var  next = $('.active').next();
@@ -15,5 +15,24 @@ frecciaDestra.click(function(){
     active.removeClass('active');
     next.addClass('active')
   }
+
+});
+frecciaSinistra.click(function(){
+  var  first = $('.first');
+  var  before = $('.active').prev();
+  var  last = $('.last');
+  var  active = $('.active');
+  var immagine = $('.images > img');
+  if (first.hasClass('active')) {
+    first.removeClass('active');
+    last.addClass('active');
+  }else{
+    active.removeClass('active');
+    before.addClass('active');
+
+  }
+
+
+
 
 });
