4b883df23b573499297f4b4e6f19b5588e5aa059
edoardocollo <edoardo.collo@gmail.com>
2020-12-01 11:27:26 +0100

    add arrow key press

--- main.js
+++ main.js
@@ -33,3 +33,38 @@ frecciaSinistra.click(function(){
   }
 
 });
+$(document).keydown(function(event){
+  var x = event.keyCode;
+  if (x == 37) {
+
+    var  first = $('.first');
+    var  before = $('.active').prev();
+    var  last = $('.last');
+    var  active = $('.active');
+    var immagine = $('.images > img');
+    if (first.hasClass('active')) {
+      first.removeClass('active');
+      last.addClass('active');
+    }else{
+      active.removeClass('active');
+      before.addClass('active');
+
+    }
+  }else if(x == 39){
+    var  first = $('.first');
+    var  next = $('.active').next();
+    var  last = $('.last');
+    var  active = $('.active');
+    var immagine = $('.images > img');
+    if (last.hasClass('active')) {
+      last.removeClass('active');
+      first.addClass('active');
+
+    }else{
+
+      active.removeClass('active');
+      next.addClass('active')
+    }
+
+  }
+});
